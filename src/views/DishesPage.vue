12cbe7cadd342f981621e3601b6b9164309e4908
vladyslav-sheremet <vladyslavsheremetdeveloper@gmail.com>
2023-03-27 16:34:33 +0300

    added some more features

--- src/views/DishesPage.vue
+++ src/views/DishesPage.vue
@@ -1,26 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
-interface Dish {
-  name?: string
-  diet?: Diet
-  status?: RecommendStatus
-}
-
-type Diet = 'vegetarian' | 'vegan' | 'gluten-free' | 'pescatarian' | 'lactose-free' | 'other'
-
-const restaurantStatusList = ['Want to Try', 'Recommended', 'Do not Recommend', 'Must try'] as const
-
-type RecommendStatus = (typeof restaurantStatusList)[number]
-
-// type RecommendStatus = 'Want to Try' | 'Recommended' | 'Do not Recommend' | 'Must try'
-//
-// const restaurantStatusList = [
-//   'Want to Try',
-//   'Recommended',
-//   'Do not Recommend',
-//   'Must try'
-// ]
+import type {Dish} from '@/types'
+import {restaurantStatusList} from '@/constant'
 
 const dishList = ref<Dish[]>([])
 const newDish = ref<Dish>({
