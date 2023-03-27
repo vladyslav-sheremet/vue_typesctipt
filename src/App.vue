40726661c3b73c7b295efe9877538b5d64bb3f42
vladyslav-sheremet <vladyslavsheremetdeveloper@gmail.com>
2023-03-27 16:03:09 +0300

    added some features

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <h1>ToEat</h1>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/dishes">Dishes</RouterLink>
       </nav>
     </div>
   </header>
@@ -26,24 +22,11 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
 }
 
 nav a {
@@ -61,10 +44,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
--- src/views/DishesPage.vue
+++ src/views/DishesPage.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-interface Restaurant {
-  name?: string
-  status?: RecommendStatus
-  dishes?: Dish[]
-}
-
 interface Dish {
-  name: string
+  name?: string
   diet?: Diet
   status?: RecommendStatus
 }
@@ -28,31 +22,30 @@ type RecommendStatus = (typeof restaurantStatusList)[number]
 //   'Must try'
 // ]
 
-const restaurantList = ref<Restaurant[]>([])
-const newRestaurant = ref<Restaurant>({
+const dishList = ref<Dish[]>([])
+const newDish = ref<Dish>({
   status: 'Want to Try'
 })
 
-function addRestaurant() {
-  restaurantList.value.push({
-    name: newRestaurant.value.name,
-    status: newRestaurant.value.status,
-    dishes: []
+function addDish() {
+  dishList.value.push({
+    name: newDish.value.name,
+    status: newDish.value.status
   })
 }
 </script>
 
 <template>
   <main>
-    <form @submit.prevent="addRestaurant">
+    <form @submit.prevent="addDish">
       <div>
-        <label for="restaurant-name">Restaurant name</label>
-        <input id="restaurant-name" type="text" v-model="newRestaurant.name" />
+        <label for="dish-name">Dish name</label>
+        <input id="dish-name" type="text" v-model="newDish.name" />
       </div>
 
       <div>
-        <label for="restaurant-status">Restaurant status</label>
-        <select name="restaurant-status" id="restaurant-status" v-model="newRestaurant.status">
+        <label for="dish-status">Dish status</label>
+        <select name="dish-status" id="dish-status" v-model="newDish.status">
           <option v-for="status in restaurantStatusList" :key="status" :value="status">
             {{ status }}
           </option>
@@ -62,9 +55,9 @@ function addRestaurant() {
       <button type="submit">Add Restaurant</button>
     </form>
     <ul>
-      <li v-for="restaurant in restaurantList" :key="restaurant.name">
-        {{ restaurant.name }}
-        {{ restaurant.status }}
+      <li v-for="dish in dishList" :key="dish.name">
+        {{ dish.name }}
+        {{ dish.status }}
       </li>
     </ul>
   </main>
